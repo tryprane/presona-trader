ai16z/eliza

implement a plugin that can retrieve Marlin TEE remote attestations

agent/src/index.ts
@@ -44,6 +44,7 @@ import { solanaPlugin } from "@ai16z/plugin-solana";
 import { aptosPlugin, TransferAptosToken } from "@ai16z/plugin-aptos";
 import { flowPlugin } from "@ai16z/plugin-flow";
 import { teePlugin } from "@ai16z/plugin-tee";
+import { teeMarlinPlugin } from "@ai16z/plugin-tee-marlin";
 import Database from "better-sqlite3";
 import fs from "fs";
 import path from "path";
@@ -415,6 +416,7 @@ export function createAgent(
                 ? flowPlugin
                 : null,
             getSecret(character, "APTOS_PRIVATE_KEY") ? aptosPlugin : null,
+            getSecret(character, "TEE_MARLIN") ? teeMarlinPlugin : null,
         ].filter(Boolean),
         providers: [],
         actions: [],

packages/plugin-tee-marlin/src/actions/remoteAttestation.ts
@@ -1,0 +1,48 @@
+import type { IAgentRuntime, Memory, State, HandlerCallback } from "@ai16z/eliza";
+
+export const remoteAttestationAction = {
+    name: "REMOTE_ATTESTATION",
+    similes: ["REMOTE_ATTESTATION", "TEE_REMOTE_ATTESTATION", "TEE_ATTESTATION"],
+    description: "Generate a remote attestation to prove that the agent is running in a TEE",
+    handler: async (
+        runtime: IAgentRuntime,
+        message: Memory,
+        _state: State,
+        _options: any,
+        callback: HandlerCallback,
+    ) => {
+        try {
+            const endpoint = runtime.getSetting("TEE_MARLIN_ATTESTATION_ENDPOINT") ?? "http://127.0.0.1:1350";
+            const response = await fetch(`${endpoint}/attestation/hex`);
+            callback({
+                text: `Here you go - ${await response.text()}`,
+                action: "NONE",
+            });
+            return true;
+        } catch (error) {
+            console.error("Failed to fetch remote attestation: ", error);
+            return false;
+        }
+    },
+    validate: async (runtime: IAgentRuntime) => {
+        return true;
+    },
+    examples: [
+        [
+            {
+                user: "user",
+                content: {
+                    text: "Attest yourself",
+                    action: "REMOTE_ATTESTATION",
+                },
+            },
+            {
+                user: "user",
+                content: {
+                    text: "Generate a remote attestation",
+                    action: "REMOTE_ATTESTATION",
+                },
+            },
+        ],
+    ],
+};

packages/plugin-tee-marlin/src/index.ts
@@ -1,0 +1,21 @@
+import { Plugin } from "@ai16z/eliza";
+import { remoteAttestationAction } from "./actions/remoteAttestation";
+
+export const teeMarlinPlugin: Plugin = {
+    name: "Marlin TEE Plugin",
+    description:
+        "TEE plugin with actions to generate remote attestations",
+    actions: [
+        /* custom actions */
+        remoteAttestationAction,
+    ],
+    evaluators: [
+        /* custom evaluators */
+    ],
+    providers: [
+        /* custom providers */
+    ],
+    services: [
+        /* custom services */
+    ],
+};

tryprane/presona-trader

agent/src/index.ts
@@ -56,6 +56,7 @@ import { createNodePlugin } from "@elizaos/plugin-node";
 import { solanaPlugin } from "@elizaos/plugin-solana";
 import { suiPlugin } from "@elizaos/plugin-sui";
 import { TEEMode, teePlugin } from "@elizaos/plugin-tee";
+import { teeMarlinPlugin } from "@elizaos/plugin-tee-marlin";
 import { tonPlugin } from "@elizaos/plugin-ton";
 import { zksyncEraPlugin } from "@elizaos/plugin-zksync-era";
 import { cronosZkEVMPlugin } from "@elizaos/plugin-cronoszkevm";
@@ -593,6 +594,7 @@ export async function createAgent(
             getSecret(character, "CRONOSZKEVM_PRIVATE_KEY")
                 ? cronosZkEVMPlugin
                 : null,
+            getSecret(character, "TEE_MARLIN") ? teeMarlinPlugin : null,
             getSecret(character, "TON_PRIVATE_KEY") ? tonPlugin : null,
             getSecret(character, "SUI_PRIVATE_KEY") ? suiPlugin : null,
             getSecret(character, "STORY_PRIVATE_KEY") ? storyPlugin : null,

packages/plugin-tee-marlin/src/actions/remoteAttestation.ts
@@ -1,0 +1,48 @@
+import type { IAgentRuntime, Memory, State, HandlerCallback } from "@elizaos/core";
+
+export const remoteAttestationAction = {
+    name: "REMOTE_ATTESTATION",
+    similes: ["REMOTE_ATTESTATION", "TEE_REMOTE_ATTESTATION", "TEE_ATTESTATION"],
+    description: "Generate a remote attestation to prove that the agent is running in a TEE",
+    handler: async (
+        runtime: IAgentRuntime,
+        message: Memory,
+        _state: State,
+        _options: any,
+        callback: HandlerCallback,
+    ) => {
+        try {
+            const endpoint = runtime.getSetting("TEE_MARLIN_ATTESTATION_ENDPOINT") ?? "http://127.0.0.1:1350";
+            const response = await fetch(`${endpoint}/attestation/hex`);
+            callback({
+                text: `Here you go - ${await response.text()}`,
+                action: "NONE",
+            });
+            return true;
+        } catch (error) {
+            console.error("Failed to fetch remote attestation: ", error);
+            return false;
+        }
+    },
+    validate: async (runtime: IAgentRuntime) => {
+        return true;
+    },
+    examples: [
+        [
+            {
+                user: "user",
+                content: {
+                    text: "Attest yourself",
+                    action: "REMOTE_ATTESTATION",
+                },
+            },
+            {
+                user: "user",
+                content: {
+                    text: "Generate a remote attestation",
+                    action: "REMOTE_ATTESTATION",
+                },
+            },
+        ],
+    ],
+};

packages/plugin-tee-marlin/src/index.ts
@@ -1,0 +1,21 @@
+import { Plugin } from "@elizaos/core";
+import { remoteAttestationAction } from "./actions/remoteAttestation";
+
+export const teeMarlinPlugin: Plugin = {
+    name: "Marlin TEE Plugin",
+    description:
+        "TEE plugin with actions to generate remote attestations",
+    actions: [
+        /* custom actions */
+        remoteAttestationAction,
+    ],
+    evaluators: [
+        /* custom evaluators */
+    ],
+    providers: [
+        /* custom providers */
+    ],
+    services: [
+        /* custom services */
+    ],
+};